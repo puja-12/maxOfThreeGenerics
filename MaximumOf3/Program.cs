601f9cca15f298773af411e7cda2cadcbaafc47b
puja <pujarana1796@gmail.com>
2022-05-06 16:26:57 +0530

    puja-added first commit

--- MaximumOf3/Program.cs
+++ MaximumOf3/Program.cs
@@ -8,13 +8,33 @@ namespace maxOfThree
         {
             Console.WriteLine("Welcome to Maximum of Three using generics!");
 
-            Console.WriteLine("Please enter the values of integers x, y and z:");
-            int x = Convert.ToInt16(Console.ReadLine());
-            int y = Convert.ToInt16(Console.ReadLine());
-            int z = Convert.ToInt16(Console.ReadLine());
-            usingInt usingInt = new usingInt();
-            int result = usingInt.Maximum(x, y, z);
-            Console.WriteLine("The Maximum of three integers is :" + result);
+            while (true)
+            {
+
+                Console.WriteLine("Choose the Program to be executed :\n1)using INT\n2)using Float\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter the values of integers x, y and z:");
+                        int x = Convert.ToInt16(Console.ReadLine());
+                        int y = Convert.ToInt16(Console.ReadLine());
+                        int z = Convert.ToInt16(Console.ReadLine());
+                        usingInt usingInt = new usingInt();
+                        int result = usingInt.Maximum(x, y, z);
+                        Console.WriteLine("The Maximum of three integers is :" + result);
+                        break;
+                    case 2:
+                        Console.WriteLine("Please enter the values of floats a, b and c:");
+                        double a = Convert.ToInt16(Console.ReadLine());
+                        double b = Convert.ToInt16(Console.ReadLine());
+                        double c = Convert.ToInt16(Console.ReadLine());
+                        usingfloat Usingfloat = new usingfloat();
+                        double result1 = Usingfloat.Maximumfloat(a, b, c);
+                        Console.WriteLine("The Maximum of three integers is :" + result1);
+                        break;
+                }
+            }
         }
     }
 }
