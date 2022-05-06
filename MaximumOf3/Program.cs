5097d0fe5144df912e8dee12bf6077bb3664c193
puja <pujarana1796@gmail.com>
2022-05-06 16:06:44 +0530

    puja-added first commit

---
+++ MaximumOf3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace maxOfThree
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum of Three using generics!");
+
+            Console.WriteLine("Please enter the values of integers x, y and z:");
+            int x = Convert.ToInt16(Console.ReadLine());
+            int y = Convert.ToInt16(Console.ReadLine());
+            int z = Convert.ToInt16(Console.ReadLine());
+            usingInt usingInt = new usingInt();
+            int result = usingInt.Maximum(x, y, z);
+            Console.WriteLine("The Maximum of three integers is :" + result);
+        }
+    }
+}
---
+++ TestCases/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace maxOfThree.Tests
+{
+    [TestClass()]
+    public class UsingIntTests
+    {
+        [TestMethod()]
+        public void MaximumTest1()
+        {
+            usingInt int1
+                = new usingInt();
+            int result = int1.Maximum(6, 5, 4);
+            Assert.AreEqual(6, result);
+
+        }
+        [TestMethod()]
+        public void MaximumTest2()
+        {
+            usingInt int2 = new usingInt();
+            int result1 = int2.Maximum(3, 7, 6);
+            Assert.AreEqual(7, result1);
+        }
+        [TestMethod()]
+        public void MaximumTest3()
+        {
+            usingInt int3 = new usingInt();
+            int result2 = int3.Maximum(3, 7, 8);
+            Assert.AreEqual(8, result2);
+        }
+    }
+}
